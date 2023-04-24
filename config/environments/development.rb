b8013a561477d31703a8e40c46ac8422da286b61
Davis-001 <davi.s@to.estudante.senai.br>
2023-04-24 17:39:39 -0300

    instalação do mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+
   #recupera a senha do devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
